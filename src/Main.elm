ed0afe82898ad386c1061e7c2e9b9893c8b465fe
dl-watson <dlarkinwatson@gmail.com>
2021-04-30 07:59:13 -0700

    client-side websocket ex

---
+++ src/Main.elm
@@ -0,0 +1,120 @@
+port module Main exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as JD
+
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- PORTS
+
+
+port sendMessage : String -> Cmd msg
+port messageReceiver : (String -> msg) -> Sub msg
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { draft : String
+  , messages : List String
+  }
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+  ( { draft = "", messages = [] }
+  , Cmd.none
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = DraftChanged String
+  | Send
+  | Recv String
+
+
+-- Use the `sendMessage` port when someone presses ENTER or clicks
+-- the "Send" button. Check out index.html to see the corresponding
+-- JS where this is piped into a WebSocket.
+--
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    DraftChanged draft ->
+      ( { model | draft = draft }
+      , Cmd.none
+      )
+
+    Send ->
+      ( { model | draft = "" }
+      , sendMessage model.draft
+      )
+
+    Recv message ->
+      ( { model | messages = model.messages ++ [message] }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+-- Subscribe to the `messageReceiver` port to hear about messages coming in
+-- from JS. Check out the index.html file to see how this is hooked up to a
+-- WebSocket.
+--
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  messageReceiver Recv
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [ text "Echo Chat" ]
+    , ul []
+        (List.map (\msg -> li [] [ text msg ]) model.messages)
+    , input
+        [ type_ "text"
+        , placeholder "Draft"
+        , onInput DraftChanged
+        , on "keydown" (ifIsEnter Send)
+        , value model.draft
+        ]
+        []
+    , button [ onClick Send ] [ text "Send" ]
+    ]
+
+
+
+-- DETECT ENTER
+
+
+ifIsEnter : msg -> JD.Decoder msg
+ifIsEnter msg =
+  JD.field "key" JD.string
+    |> JD.andThen (\key -> if key == "Enter" then JD.succeed msg else JD.fail "some other key")
